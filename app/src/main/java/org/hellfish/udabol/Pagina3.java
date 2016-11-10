61906c4e66e5ea6befe327ec49b0bd342c01084f
Hellfish <boriscoronado5@gmail.com>
2016-11-10 10:59:26 -0400

    Modificacion 10/11/2016

--- app/src/main/java/org/hellfish/udabol/Pagina3.java
+++ app/src/main/java/org/hellfish/udabol/Pagina3.java
@@ -1,7 +1,9 @@
 package org.hellfish.udabol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pagina3 extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Pagina3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina3);
+    }
+
+    public void a1(View view){
+        Intent i = new Intent(this,A1.class);
+        startActivity(i);
+    }
+
+    public void a2(View view){
+        Intent i = new Intent(this,A2.class);
+        startActivity(i);
     }
 }
